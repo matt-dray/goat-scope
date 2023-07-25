4425c992c97332eb0e07e745a3c6155a8cbbe08b
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2023-07-25 21:12:07 +0100

    Add necessary files

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# goat-scope
+
+<!-- badges: start -->
+[![Project Status: Inactive – The project has reached a stable, usable state but is no longer being actively developed; support/maintenance will be provided as time allows.](https://www.repostatus.org/badges/latest/inactive.svg)](https://www.repostatus.org/#inactive)
+[![Blog post](https://img.shields.io/badge/rostrum.blog-post-008900?labelColor=000000&logo=data%3Aimage%2Fgif%3Bbase64%2CR0lGODlhEAAQAPEAAAAAABWCBAAAAAAAACH5BAlkAAIAIf8LTkVUU0NBUEUyLjADAQAAACwAAAAAEAAQAAAC55QkISIiEoQQQgghRBBCiCAIgiAIgiAIQiAIgSAIgiAIQiAIgRAEQiAQBAQCgUAQEAQEgYAgIAgIBAKBQBAQCAKBQEAgCAgEAoFAIAgEBAKBIBAQCAQCgUAgEAgCgUBAICAgICAgIBAgEBAgEBAgEBAgECAgICAgECAQIBAQIBAgECAgICAgICAgECAQECAQICAgICAgICAgEBAgEBAgEBAgICAgICAgECAQIBAQIBAgECAgICAgIBAgECAQECAQIBAgICAgIBAgIBAgEBAgECAgECAgICAgICAgECAgECAgQIAAAQIKAAAh%2BQQJZAACACwAAAAAEAAQAAAC55QkIiESIoQQQgghhAhCBCEIgiAIgiAIQiAIgSAIgiAIQiAIgRAEQiAQBAQCgUAQEAQEgYAgIAgIBAKBQBAQCAKBQEAgCAgEAoFAIAgEBAKBIBAQCAQCgUAgEAgCgUBAICAgICAgIBAgEBAgEBAgEBAgECAgICAgECAQIBAQIBAgECAgICAgICAgECAQECAQICAgICAgICAgEBAgEBAgEBAgICAgICAgECAQIBAQIBAgECAgICAgIBAgECAQECAQIBAgICAgIBAgIBAgEBAgECAgECAgICAgICAgECAgECAgQIAAAQIKAAA7)](https://www.rostrum.blog/2019/06/20/goat-scope/)
+<!-- badges: end -->
+
+Generate a 'trelliscope' for [The Mountain Goats](https://en.wikipedia.org/wiki/The_Mountain_Goats) songs with data from Spotify.
+
+Published originally in [a blog post](https://www.rostrum.blog/2019/06/20/goat-scope/) on 2019-06-20. Put into its own repo in July 2023 so that [the output](https://matt-dray.github.io/goat-scope) can be shared more easily via GitHub Pages. 
+
+Note that the data set was collected as part of [an earlier blog post](https://www.rostrum.blog/2019/04/25/gen-tmg-lyrics/), published 2019-04-25.
+
+It will not surprise any Mountain Goats fans that they've [released a lot more material](https://en.wikipedia.org/wiki/The_Mountain_Goats_discography) since then, lol.
---
+++ goat-scope.R
@@ -0,0 +1,52 @@
+# Generating a 'trelliscope' for Mountain Goats songs with data from Spotify.
+# See README for explanation.
+
+library(trelliscopejs)
+library(dplyr)
+library(readr)
+library(tidyr)
+
+raw_goat <- read_rds("data/goat_discography.RDS")  # pre-gathered dataset
+
+small_goat <- raw_goat %>% 
+  unnest(available_markets) %>%
+  filter(available_markets == "US") %>%
+  select(
+    track_name, album_name, track_n, album_release_year,  # track detail
+    duration_ms, key_mode, time_signature, # musical info
+    danceability, energy, speechiness, acousticness,  # audio features
+    instrumentalness, liveness, valence, loudness  # audio features
+  ) %>%
+  arrange(desc(energy))
+
+goat_pics <- raw_goat %>%
+  unnest(album_images) %>%  # unnest dataframe of URLs
+  filter(width == 640) %>%  # just the largest images
+  select(album_name, url) %>%  # simplify dataset
+  distinct(album_name, .keep_all = TRUE)  # one unique entry per album
+
+small_goat_pics <- left_join(small_goat, goat_pics, by = "album_name")
+
+prepared_goat <- small_goat_pics %>% 
+  mutate(panel = img_panel(url)) %>%  # identify as viz for panel
+  rename_all(tools::toTitleCase) %>%
+  rename(
+    Track = Track_name,
+    Album = Album_name,
+    `Track #` = Track_n,
+    Year = Album_release_year,
+    `Duration (ms)` = Duration_ms,
+    `Key mode` = Key_mode,
+    `Time sig` = Time_signature
+  ) %>% 
+  select(-Url)
+
+trelliscope(
+  prepared_goat,
+  name = "The Mountain Goats discography",
+  desc = "Explore the Mountain Goats backcatalogue and filter and sort by audio features",
+  md_desc = "[The Mountain Goats](http://www.mountain-goats.com/) are a band. Data were collected from [Genius](https://genius.com/) and [Spotify](https://www.spotify.com/) APIs using the [{genius}](https://github.com/josiahparry/genius) and [{spotifyr}](https://www.rcharlie.com/spotifyr/) R packages, respectively.",
+  nrow = 2, ncol = 5,  # arrangement of panels
+  state = list(labels = c("Track", "Album", "Track #", "Year", "Energy")),  # display on panels
+  path = "output"
+)
